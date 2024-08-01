a044e412ae33a9a3c95ef2e1ceb39edf0dbf76fc
shyam sunder v <shyam@shyams-MacBook-Air.local>
2024-08-01 18:43:09 +0530

    admin updated

--- adminfrontend/src/shadcn/ui/3d-card.tsx
+++ adminfrontend/src/shadcn/ui/3d-card.tsx
@@ -62,7 +62,7 @@ export const CardContainer = ({
           onMouseMove={handleMouseMove}
           onMouseLeave={handleMouseLeave}
           className={cn(
-            "flex items-center justify-center relative transition-all duration-200 ease-linear",
+            "flex items-center justify-center h-full w-screen relative transition-all duration-200 ease-linear",
             className
           )}
           style={{
@@ -86,7 +86,7 @@ export const CardBody = ({
   return (
     <div
       className={cn(
-        "h-96 w-96 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
+        "h-96 w-2/3 [transform-style:preserve-3d]  [&>*]:[transform-style:preserve-3d]",
         className
       )}
     >
